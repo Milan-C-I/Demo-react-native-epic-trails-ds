d3086c06e23bbab59806f999c0a373be2ef14a0b
Milan-C-I <milancheriyamanep@gmail.com>
2025-04-25 16:11:15 +0530

    changes

--- app/design3/index.tsx
+++ app/design3/index.tsx
@@ -1,17 +1,21 @@
 import { useRouter } from "expo-router";
 import { Image, Text, View } from "react-native";
-import { BottomNav, Iconography, NavTabs, RectButton } from "reactnativeepictrailsds";
+import {
+  BottomNav,
+  Iconography,
+  NavTabs,
+  RectButton,
+} from "reactnativeepictrailsds";
 export default function Index() {
-
   const router = useRouter();
 
   const items = [
-    {icon: 'home',label: 'Home',},
-    {icon: 'speedometer',label: 'Trips',},
-    {icon: 'add',label: 'Create',},
-    {icon: 'chatbubble',label: 'Chat',},
-    {icon: 'person',label: 'Me',}
-  ]
+    { icon: "home", label: "Home" },
+    { icon: "speedometer", label: "Trips" },
+    { icon: "add", label: "Create" },
+    { icon: "chatbubble", label: "Chat" },
+    { icon: "person", label: "Me" },
+  ];
 
   return (
     <View
@@ -25,7 +29,7 @@ export default function Index() {
       <View className="w-[80%] m-12">
         <NavTabs tabs={["Your Trips", "Archived"]} />
       </View>
-      <RectButton label="click" onPress={() => router.push('/design4')}/>
+      <RectButton label="click" onPress={() => router.push("/design4")} />
       <View className="w-[80%] m-4 p-4 bg-gray-100">
         <View style={{ height: 180, backgroundColor: "#abdbe3" }}>
           <Image
@@ -33,7 +37,7 @@ export default function Index() {
             source={require("@/assets/images/No-Rides-Logged-Yet.png")}
             resizeMode="cover"
           />
-          <View className="absolute top-2 left-2 flex-row items-center" >
+          <View className="absolute top-2 left-2 flex-row items-center">
             <Iconography type="avatar" initials="V" />
             <Text className="font-semibold mx-4 text-base">Vignesh</Text>
           </View>
@@ -48,10 +52,12 @@ export default function Index() {
                 zIndex: 10 + i, // Stack in order
               }}
             >
-              <Iconography type="avatar"  initials={char} />
+              <Iconography type="avatar" initials={char} />
             </View>
           ))}
-          <Text className="my-1 mx-2 text-gray-600">Preetham, Vignesh & 1 other</Text>
+          <Text className="my-1 mx-2 text-gray-600">
+            Preetham, Vignesh & 1 other
+          </Text>
         </View>
       </View>
       <View className="w-[80%] mx-8 my-2 p-4 bg-gray-100">
@@ -61,7 +67,7 @@ export default function Index() {
             source={require("@/assets/images/No-Rides-Logged-Yet.png")}
             resizeMode="cover"
           />
-          <View className="absolute top-2 left-2 flex-row items-center" >
+          <View className="absolute top-2 left-2 flex-row items-center">
             <Iconography type="avatar" initials="V" />
             <Text className="font-semibold mx-4 text-base">Vignesh</Text>
           </View>
@@ -76,10 +82,12 @@ export default function Index() {
                 zIndex: 10 + i, // Stack in order
               }}
             >
-              <Iconography type="avatar"  initials={char} />
+              <Iconography type="avatar" initials={char} />
             </View>
           ))}
-          <Text className="my-1 mx-2 text-gray-600">Preetham, Vignesh & 1 other</Text>
+          <Text className="my-1 mx-2 text-gray-600">
+            Preetham, Vignesh & 1 other
+          </Text>
         </View>
       </View>
       <BottomNav items={items} />
