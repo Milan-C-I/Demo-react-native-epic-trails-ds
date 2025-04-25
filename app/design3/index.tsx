20a5f9e0a76115a3ea42093fd7249b83d2553165
Milan-C-I <milancheriyamanep@gmail.com>
2025-04-25 14:54:58 +0530

    first commit

---
+++ app/design3/index.tsx
@@ -0,0 +1,88 @@
+import { useRouter } from "expo-router";
+import { Image, Text, View } from "react-native";
+import { BottomNav, Iconography, NavTabs, RectButton } from "reactnativeepictrailsds";
+export default function Index() {
+
+  const router = useRouter();
+
+  const items = [
+    {icon: 'home',label: 'Home',},
+    {icon: 'speedometer',label: 'Trips',},
+    {icon: 'add',label: 'Create',},
+    {icon: 'chatbubble',label: 'Chat',},
+    {icon: 'person',label: 'Me',}
+  ]
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <View className="w-[80%] m-12">
+        <NavTabs tabs={["Your Trips", "Archived"]} />
+      </View>
+      <View className="w-[80%] m-4 p-4 bg-gray-100">
+        <View style={{ height: 180, backgroundColor: "#abdbe3" }}>
+          <Image
+            style={{ width: "100%", height: "100%" }}
+            source={require("@/assets/images/No-Rides-Logged-Yet.png")}
+            resizeMode="cover"
+          />
+          <View className="absolute top-2 left-2 flex-row items-center" >
+            <Iconography type="avatar" initials="V" />
+            <Text className="font-semibold mx-4 text-base">Vignesh</Text>
+          </View>
+        </View>
+        <Text className="my-2 font-medium text-base">Trip to Ladak</Text>
+        <View className="flex-row">
+          {["S", "T", "A"].map((char, i) => (
+            <View
+              key={i}
+              style={{
+                marginLeft: i === 0 ? 0 : -15, // Overlap
+                zIndex: 10 + i, // Stack in order
+              }}
+            >
+              <Iconography type="avatar"  initials={char} />
+            </View>
+          ))}
+          <Text className="my-1 mx-2 text-gray-600">Preetham, Vignesh & 1 other</Text>
+        </View>
+      </View>
+      <View className="w-[80%] mx-8 my-2 p-4 bg-gray-100">
+        <View style={{ height: 180, backgroundColor: "#abdbe3" }}>
+          <Image
+            style={{ width: "100%", height: "100%" }}
+            source={require("@/assets/images/No-Rides-Logged-Yet.png")}
+            resizeMode="cover"
+          />
+          <View className="absolute top-2 left-2 flex-row items-center" >
+            <Iconography type="avatar" initials="V" />
+            <Text className="font-semibold mx-4 text-base">Vignesh</Text>
+          </View>
+        </View>
+        <Text className="my-2 font-medium text-base">Trip to Kashmir</Text>
+        <View className="flex-row">
+          {["S", "T", "A"].map((char, i) => (
+            <View
+              key={i}
+              style={{
+                marginLeft: i === 0 ? 0 : -15, // Overlap
+                zIndex: 10 + i, // Stack in order
+              }}
+            >
+              <Iconography type="avatar"  initials={char} />
+            </View>
+          ))}
+          <Text className="my-1 mx-2 text-gray-600">Preetham, Vignesh & 1 other</Text>
+        </View>
+      </View>
+      <RectButton label="click" onPress={() => router.push('/design4')}/>
+      <BottomNav items={items} />
+    </View>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View } from "react-native";
+import { SocialLogin, TypeDoc } from "reactnativeepictrailsds";
+import { NormalInput } from "reactnativeepictrailsds";
+import EpicTrailsLogo from "@/assets/images/Epictrails-logo.svg";
+import { useRouter } from "expo-router";
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View style={{width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}}>
+      <EpicTrailsLogo width={250} height={120} />
+      <Text className="text-xl font-bold">Login</Text>
+      <View className="w-[80%] m-4">
+      <Text className="my-2">Login</Text>
+      <NormalInput placeholder="Email" Size="full"/>
+      <Text className="my-2 text-gray-600">user@email.com</Text>
+      <Text className="my-2">Password</Text>
+      <NormalInput Size="full"/>
+      </View>
+      <TypeDoc  buttonLabel="Login" label="I agree to the terms and conditions." size="large"
+        onClick={() => router.push('/design2')}
+      />
+      <Text className="font-medium">or</Text>
+     <SocialLogin email={false} google apple
+        onEmailSignIn={() => console.log('Email sign-in')}
+        onGoogleSignIn={() => console.log('Google sign-in')}
+        onFacebookSignIn={() => console.log('Facebook sign-in')}
+        onAppleSignIn={() => console.log('Apple sign-in')}
+      />
+    </View>
+  );
+}
